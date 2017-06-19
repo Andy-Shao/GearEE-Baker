8bbcdfee9242fb1643316a4c3cb2c723ec4fd3e2
Andy.Shao <hj13035952619@126.com>
2017-06-19 15:26:10 +0800

    add basic annotation

---
+++ src/main/java/com/github/andyshaox/baker/SourcerEnable.java
@@ -0,0 +1,14 @@
+package com.github.andyshaox.baker;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 19 Jun 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public @interface SourcerEnable {
+
+}
