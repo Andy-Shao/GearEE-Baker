15790b4358d69ee5131ab803faa2f61d95ef4cf5
Andy.Shao <hj13035952619@126.com>
2017-06-20 11:35:17 +0800

    first register

---
+++ baker-register/spring-cloud-eureka/src/main/java/com/github/andyshaox/baker/register/spring/cloud/EurekaService.java
@@ -0,0 +1,22 @@
+package com.github.andyshaox.baker.register.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 20 Jun 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaService {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaService.class , args);
+    }
+}
