fa4c08fa88a842a3fc4ac6852c93cbca8c49056d
Andy.Shao <hj13035952619@126.com>
2017-06-21 15:00:49 +0800

    adjust configure

---
+++ baker-conf/core/src/main/java/com/github/andyshaox/baker/conf/PackagerStatusReceiveService.java
@@ -0,0 +1,14 @@
+package com.github.andyshaox.baker.conf;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 21 Jun 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public interface PackagerStatusReceiveService {
+
+}
---
+++ baker-packager/core/src/main/java/com/github/andyshaox/baker/packager/Task.java
@@ -0,0 +1,15 @@
+package com.github.andyshaox.baker.packager;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 21 Jun 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public interface Task {
+    String taskName();
+    boolean isRunable();
+}
