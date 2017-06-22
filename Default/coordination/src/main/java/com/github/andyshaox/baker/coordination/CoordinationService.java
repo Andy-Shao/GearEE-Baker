f2738c176b98fae175f5369185a0fb6512046b06
Andy.Shao <hj13035952619@126.com>
2017-06-22 10:54:42 +0800

    adjust

--- Default/coordination/src/main/java/com/github/andyshaox/baker/coordination/CoordinationService.java
+++ Default/coordination/src/main/java/com/github/andyshaox/baker/coordination/CoordinationService.java
@@ -1,11 +1,13 @@
 package com.github.andyshaox.baker.coordination;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableAutoConfiguration
 public class CoordinationService {
     public static void main(String[] args) {
         SpringApplication.run(CoordinationService.class , args);
