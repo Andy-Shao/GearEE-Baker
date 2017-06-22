1075cab7726651c95c14c4ecca46d92a2fa6c5be
Andy.Shao <hj13035952619@126.com>
2017-06-22 10:41:50 +0800

    adjust

--- Default/eureka/src/main/java/com/github/andyshaox/baker/register/spring/cloud/EurekaService.java
+++ Default/eureka/src/main/java/com/github/andyshaox/baker/register/spring/cloud/EurekaService.java
@@ -1,6 +1,7 @@
 package com.github.andyshaox.baker.register.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -15,7 +16,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-//@EnableAutoConfiguration
+@EnableAutoConfiguration
 public class EurekaService {
     public static void main(String[] args) {
         SpringApplication.run(EurekaService.class , args);
